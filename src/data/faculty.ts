7e2c8c600a3914c3bb7aff18bdbef4bf3d5f1bec
KuHarish <hnthilagar@gmail.com>
2026-07-07 15:01:48 +0530

    feat: automate faculty data extraction and implement a searchable directory page

---
+++ src/data/faculty.ts
@@ -0,0 +1,152 @@
+export const FACULTY = [
+  {
+    "name": "Dr. Balaji N",
+    "designation": "Principal/Professor",
+    "qualification": "M.E., Ph.D.,",
+    "image": "/faculty-images/dr r balaji.jpeg"
+  },
+  {
+    "name": "Dr. Soundararajan S",
+    "designation": "Vice-Principal/Professor",
+    "qualification": "M.E., Ph.D.,",
+    "image": "/faculty-images/Dr.soundrajan .jpeg"
+  },
+  {
+    "name": "Dr. Venkadesh R",
+    "designation": "HOD/Professor",
+    "qualification": "M. Tech., Ph.D.,",
+    "image": "/faculty-images/venkdesh.jpeg"
+  },
+  {
+    "name": "Dr. Guruvammal S",
+    "designation": "Professor",
+    "qualification": "M.E., Ph.D.,",
+    "image": "/faculty-images/gurvaammal.jpeg"
+  },
+  {
+    "name": "Mr. Raja R",
+    "designation": "Associate Professor",
+    "qualification": "M.E., (Ph. D).,",
+    "image": "/faculty-images/raja r.jpeg"
+  },
+  {
+    "name": "Dr. Sarravanaprabhu A.M",
+    "designation": "Assistant Professor",
+    "qualification": "M.E., Ph.D.,",
+    "image": "/faculty-images/sarvana prbhu.jpeg"
+  },
+  {
+    "name": "Mr. Anbumani A",
+    "designation": "Assistant Professor",
+    "qualification": "M., Tech.,",
+    "image": "/faculty-images/anbumani.jpeg"
+  },
+  {
+    "name": "Mr. Sathiya Jebasundar J",
+    "designation": "Assistant Professor",
+    "qualification": "M.E.,",
+    "image": "/faculty-images/sathiya jebasundar.jpeg"
+  },
+  {
+    "name": "Mr. Retheesh D",
+    "designation": "Assistant Professor",
+    "qualification": "M.E., (Ph. D).,",
+    "image": "/faculty-images/retheesh.jpeg"
+  },
+  {
+    "name": "Mr. Nithinkumar R",
+    "designation": "Assistant Professor",
+    "qualification": "M. Tech., (Ph. D).,",
+    "image": "/faculty-images/nithin.jpeg"
+  },
+  {
+    "name": "Mrs. Ponsangeetha A",
+    "designation": "Assistant Professor",
+    "qualification": "M.E., (Ph. D).,",
+    "image": "/faculty-images/ponsangeetha .jpeg"
+  },
+  {
+    "name": "Mrs. Pratheeba R S",
+    "designation": "Assistant Professor",
+    "qualification": "M.E., (Ph. D).,",
+    "image": "/faculty-images/pratheeba.jpeg"
+  },
+  {
+    "name": "Mrs. Joice Ruby J",
+    "designation": "Assistant Professor",
+    "qualification": "M.E., (Ph. D).,",
+    "image": "/faculty-images/joy ruby.jpeg"
+  },
+  {
+    "name": "Mrs. Bhavani Chandana",
+    "designation": "Assistant Professor",
+    "qualification": "M. Tech.,",
+    "image": "/faculty-images/kanchana.jpeg"
+  },
+  {
+    "name": "Mrs. Renuga R",
+    "designation": "Assistant Professor",
+    "qualification": "M.E., (Ph. D).,",
+    "image": "/faculty-images/renuga r.jpeg"
+  },
+  {
+    "name": "Mr. Iraniya Pandiyan M",
+    "designation": "Assistant Professor",
+    "qualification": "M.E., (Ph. D).,",
+    "image": "/faculty-images/pandian.jpeg"
+  },
+  {
+    "name": "Mr. Kannan G",
+    "designation": "Assistant Professor",
+    "qualification": "M.E., (Ph. D).,",
+    "image": "/faculty-images/kanan.jpeg"
+  },
+  {
+    "name": "Ms. Anitha T",
+    "designation": "Assistant Professor",
+    "qualification": "M.E.,",
+    "image": "/faculty-images/anitha.jpeg"
+  },
+  {
+    "name": "Mrs. Merlin Rosia J",
+    "designation": "Assistant Professor",
+    "qualification": "M.E., (Ph. D).,",
+    "image": "/faculty-images/rose.jpeg"
+  },
+  {
+    "name": "Mrs. Kavitha T",
+    "designation": "Assistant Professor",
+    "qualification": "M.E., (Ph. D).,",
+    "image": "/faculty-images/kavitha.jpeg"
+  },
+  {
+    "name": "Ms. Konduru Lakshmi Mounika K",
+    "designation": "Assistant Professor",
+    "qualification": "M.E.,",
+    "image": "/faculty-images/konduru lakshmi Mounika.jpeg"
+  },
+  {
+    "name": "Mrs. Kanchana D",
+    "designation": "Assistant Professor",
+    "qualification": "M.E., (Ph. D).,",
+    "image": "/faculty-images/kanchana.jpeg"
+  },
+  {
+    "name": "Ms. Mary Flavia Lucas",
+    "designation": "Assistant Professor",
+    "qualification": "M.E.,",
+    "image": "/faculty-images/mary flavia.jpeg"
+  },
+  {
+    "name": "Mrs. Saranya G",
+    "designation": "Assistant Professor",
+    "qualification": "M.E.,",
+    "image": "/faculty-images/saranvaya.jpeg"
+  },
+  {
+    "name": "Mrs. Sindhu P S",
+    "designation": "Assistant Professor",
+    "qualification": "M.E.,",
+    "image": "/faculty-images/sidhu.jpeg"
+  }
+];
